1a3c7b437434bb5d56e0834a4ed68d573b4bf162
syniuhin <agony404@gmail.com>
2016-05-28 18:12:24 +0300

    Restructure.

--- lab3/plot.R
+++ lab3/plot.R
@@ -1,8 +1,16 @@
-get.dataframe <- function(n, k1, k2) {
-  rbind(data.frame(xemp=f.dist(n=n, k1=k1, k2=k2),
-                   y='Рівномірний - генератор Ковею'),
+df.custom.all <- function(n, k1, k2) {
+  data.frame(xemp=f.dist(n=n, k1=k1, k2=k2),
+             y='Рівномірний - генератор Ковею')
+}
+
+df.custom.beta <- function(n, k1, k2) {
+  rbind(df.custom.all(n, k1, k2),
         data.frame(xemp=f.dist(n=n, k1=k1, k2=k2, unif.custom=F),
-                   y='Рівномірний - системний генератор'),
+                   y='Рівномірний - системний генератор'))
+}
+
+df.all <- function(n, k1, k2) {
+  rbind(df.custom.beta(n, k1, k2),
         data.frame(xemp=f.dist(n=n, k1=k1, k2=k2, unif.custom=F,
                                beta.custom=F),
                    y='Бета - системний генератор'),
@@ -10,9 +18,9 @@ get.dataframe <- function(n, k1, k2) {
                    y='Фішера - системний генератор'))
 }
 
-f.plot.compare <- function(n=10000, k1, k2, xmax=5, precision=.1) {
-  f.df <- get.dataframe(n, k1, k2)
+f.plot <- function(fun.df=df.all, n=10000, k1, k2, xmax=5, precision=.1) {
+  f.df <- fun.df(n, k1, k2)
   ggplot(f.df, aes(x=xemp, ..density.., colour = y)) +
-    geom_freqpoly(alpha = .5, binwidth = precision, position="identity") +
+    geom_area(alpha = .1, binwidth = precision, position="identity", stat="bin") +
     xlim(0, xmax)
 }
